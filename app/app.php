07d675b59965ad1f1c82fc8be2b7b032e131af7a
martin cartledge <martincartledge@icloud.com>
2016-10-03 11:15:56 -0700

    add routes to get, post, update, and delete shortcut

--- app/app.php
+++ app/app.php
@@ -23,22 +23,34 @@
     'twig.path' => __DIR__.'/../views'
     ));
 
-  //loads actual twig file
+  //get inputs
     $app->get("/", function() use ($app) {
         return $app['twig']->render("home.html.twig", array('snippets' => Snippet::getAll()));
     });
 
-    // get text input and shortcut input
-    $app->get("/snippets", function() use ($app) {
-        return $app['twig']->render("home.html.twig", array('snippets' => Snippet::getAll()));
-      });
-
     // post text input and shortcut input
     $app->post("new_snippet", function() use ($app) {
         $shortcut = $_POST['shortcut'];
+        $text = $_POST['text'];
+        $new_snippet = new Snippet($shortcut, $text);
+        $new_snippet->save();
+        return $app['twig']->render("home.html.twig", array('snippets' => Snippet::getAll()));
 
+    // update snippet
+    $app->patch("/snippets/{id}", function($id) use ($app) {
+        $new_snippet = $_POST['new_snippet'];
+        $snippet = Snippet::find($id);
+        $snippet->update($new_snippet);
+        return $app['twig']->render("home.html.twig", array('snippets' => Snippet::getAll()));
+        });
     });
 
+    // delete snippet
+    $app->delete("delete_snippet/{id}", function($id) use ($app) {
+        $snippet = Snippet::find($id);
+        $snippet->delete();
+        return $app['twig']->render("home.html.twig", array('snippets' => Snippet::getAll()));
+    });
 
 
     return $app;
