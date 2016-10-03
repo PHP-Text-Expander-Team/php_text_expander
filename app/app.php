cbeb1ac9f4c841591f2151b4883d5d429cbf248a
Matthew Brandenburg <matt.bran87@gmail.com>
2016-10-03 10:38:15 -0700

    add app file with some routing

--- app/app.php
+++ app/app.php
@@ -1,24 +1,19 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/template.php";
+    require_once __DIR__."/../src/Snippet.php";
 
     //Epicodus
-    $server = 'mysql:host=localhost;dbname=best_restaurants_test';
+    $server = 'mysql:host=localhost;dbname=expander';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    //home mac
-    // $server = 'mysql:host=localhost:8889;dbname=best_restaurants';
+    // home mac
+    // $server = 'mysql:host=localhost:8889;dbname=expander';
     // $username = 'root';
     // $password = 'root';
     // $DB = new PDO($server, $username, $password);
-
-    // session_start();
-    // if (empty($_SESSION['collection'])) {
-    //     $_SESSION['collection'] = array();
-    // }
 
     $app = new Silex\Application();
 
@@ -30,13 +25,22 @@
 
   //loads actual twig file
     $app->get("/", function() use ($app) {
-      return $app['twig']->render("home.html.twig");
+        return $app['twig']->render("home.html.twig", array( 'results' => getAll()));
     });
 
-  //loads basic php
-    $app->get("/test", function() use ($app) {
-      return 'test variables here';
+    // get text input and shortcut input
+
+    $app->get("/snippets", function() use ($app) {
+        $shortcut = $_POST[];
+        $text = $_POST[];
+        $new_snippet = new Snippet($shortcut, $text);
+        $new_snippet->save();
+        return $app['twig']->render("home.html.twig", array( 'results' => getAll()));
     });
+
+    // post text input and shortcut input
+
+
 
     return $app;
 ?>
