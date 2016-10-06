7fc124b8a97b3316f56c8f573e1284bb6790658b
martin cartledge <martincartledge@icloud.com>
2016-10-06 10:49:29 -0700

    resolve merge conflicts

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,9 +1,9 @@
-//<!-- Back End -->
+// <!-- Back End -->
 console.log('basic javascript is functioning')
 
-//<!-- Front End  -->
-$(document).ready(function(){
-if (jQuery) {
+// <!-- Front End  -->
+$ (document).ready(function(){
+    if (jQuery) {
 console.log('jQuery 3.1.0 is loaded');
 } else {
 console.log('jQuery is not loaded');
